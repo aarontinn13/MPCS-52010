9632ba65021d9f342c2e05152fcb97bd6a04015e
Aaron <tinn.3@buckeyemail.osu.edu>
2019-01-17 22:00:46 -0600

    saving

--- project1/Emulator.py
+++ project1/Emulator.py
@@ -18,54 +18,69 @@ from sys import exit
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-c', action='store', default=65536, dest='cache_size',help='size of the cache (default: 65,536 Bytes)')
-parser.add_argument('-b', action='store', default=64, dest='block_size', help='size of a block (default: 64 Bytes)')
-parser.add_argument('-n', action='store', default=2, dest='n_way', help='n-way associativity (default: 2)')
-parser.add_argument('-r', action='store', default='LRU', dest='replacement', help='replacement policy (default: LRU)')
-parser.add_argument('-a', action='store', default='mxm-block', dest='algorithm', help='algorithm to test (default: mxm-block)')
-parser.add_argument('-d', action='store', default=480, dest='dimension', help='dimension of the matrix (default: 480)')
+parser.add_argument('-c', action='store', default=65536, type=int, dest='cache_size',help='size of the cache (default: 65,536 Bytes)')
+parser.add_argument('-b', action='store', default=64, type=int, dest='block_size', help='size of a block (default: 64 Bytes)')
+parser.add_argument('-n', action='store', default=2, type=int, dest='n_way', help='n-way associativity (default: 2 blocks/set)')
+parser.add_argument('-r', action='store', default='LRU', type=str, dest='replacement', help='replacement policy [FIFO, LRU] (default: LRU)')
+parser.add_argument('-a', action='store', default='mxm-block', type=str, dest='algorithm', help='algorithm to test [daxpy, mxm, mxm-block] (default: mxm-block)')
+parser.add_argument('-d', action='store', default=480, type=int, dest='dimension', help='dimension of the matrix (default: 480 floats)')
 parser.add_argument('-p', action='store_true', default=False, dest='print_',help='enables printing of the value')
-parser.add_argument('-f', action='store', default=32, dest='blocking_factor',help='blocking factor of mxm-block (default: 32)')
-parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+parser.add_argument('-f', action='store', default=32, type=int, dest='blocking_factor',help='blocking factor of mxm-block (default: 32)')
+parser.add_argument('-v', action='store', default=3, type=int, dest='d_value',help='random d-value for daxpy algorithm (default: 3)')
 
 results = parser.parse_args()
+
 cache_size = results.cache_size
 block_size = results.block_size
-n_way
-replacement
-algorithm
-dimension
-print_
-blocking_factor
+n_way = results.n_way
+replacement = results.replacement
+algorithm = results.algorithm
+dimension = results.dimension
+print_ = results.print_
+blocking_factor = results.blocking_factor
+d_value = results.d_value                      #d-value for daxpy algorithm
+float_size = 8                                 #default float size
+floats_in_block = float(block_size/float_size) #number of floats in a block
+blocks_in_cache = float(cache_size/block_size) #number of blocks in the cache
+sets_in_cache = float(blocks_in_cache/n_way)   #number of sets in the cache (sets = 1 then fully associative, blocks=sets then DMC, else n-way)
 
-
-
-
-
-'''
-print('cache_size =', results.cache_size)
-print('block_size =', results.block_size)
-print('n_way =', results.n_way)
-print('replacement =', results.replacement)
-print('algorithm =', results.algorithm)
-print('dimension =', results.dimension)
-print('print =', results.print)
-print('blocking_factor =', results.blocking_factor)
-'''
-
-#prepare calculations
-
+'''prepare calculations'''
 #check if block_size is a multiple of 8
-if log(results.block_size,8).is_integer():
+if floats_in_block.is_integer():
     pass
 else:
     exit("Error: Block size needs to be a multiple of 8 in order to fit even number of 8 byte floats!")
 
 #check if cache size is valid
-if float(resultscache_size/results.block_size.is_integer():
+if blocks_in_cache.is_integer():
     pass
 else:
-    exit("Error: cache size needs to be a multiple of 2!")
+    exit("Error: cache size needs to be a multiple of block size in order to fit even number of blocks!")
+
+#check if set size is valid
+if sets_in_cache.is_integer():
+    pass
+else:
+    exit("Error: n-way needs to divide blocks in the cache evenly to have even sets in the cache!")
+
+if sets_in_cache > 1:
+    pass
+else:
+    exit("Error: n-way is greater than number of blocks in the cache!")
+
+if algorithm == 'daxpy':
+
+    #construct arrays of Addresses of length(dimension)
+    a = list(range(0, dimension*float_size, float_size))
+    b = list(range(dimension*float_size, 2*dimension*float_size, float_size))
+    c = list(range(2*dimension*float_size, 3*dimension*float_size, float_size))
+
+    #create RAM
+    ram = RAM()
+    address = Address()
+    cache = Cache()
+    cpu = CPU()
+    datablock = DataBlock(results.block_size)
 
 
 
@@ -73,50 +88,5 @@ else:
 
 
 
-'''
-print('cache_size =', log(results.cache_size,2).is_integer())
-print('block_size =', log(results.block_size,2))
-print('n_way =', results.n_way)
-print('replacement =', results.replacement)
-print('algorithm =', results.algorithm)
-print('dimension =', results.dimension)
-print('print =', results.print)
-print('blocking_factor =', results.blocking_factor)
-address = Address()
-cache = Cache()
-cpu = CPU()
-datablock = DataBlock(results.block_size)
-ram = RAM()
-'''
-
-#pass arguments through instances
 
 
-
-
-
-
-
-
-'''
-def bitwise(x,y):
-
-    z = max(len(bin(x).partition('b')[2]), len(bin(y).partition('b')[2]))
-    z1 = min(len(bin(x).partition('b')[2]), len(bin(y).partition('b')[2]))
-    rem = z - z1
-
-
-    print(bin(x).partition('b')[2])
-    print(bin(y).partition('b')[2])
-
-
-
-
-    print(bin(x&y).partition('b')[2])
-    print(bin(x|y).partition('b')[2])
-
-
-
-bitwise(1,6)
-
-'''
